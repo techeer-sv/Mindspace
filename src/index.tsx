a4ffc7d39e9de2c60fd7e72c34a8edf3149c4830
teawon <xodnjsvkvk9@gmail.com>
2023-04-17 00:22:50 +0900

    chore : reset.scss 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'index.css';
+import 'styles/reset.scss';
+import 'styles/global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,

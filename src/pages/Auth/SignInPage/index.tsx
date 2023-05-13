1fd0894c6367e555c61fe7c10e7c3b8151b2fe2d
teawon <xodnjsvkvk9@gmail.com>
2023-05-14 04:14:52 +0900

    feat : 로그인 api 연결

--- src/pages/Auth/SignInPage/index.tsx
+++ src/pages/Auth/SignInPage/index.tsx
@@ -1,20 +1,74 @@
-import styles from './../Auth.module.scss';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import FormBox from '../components/FormBox';
 import FormButton from '../components/FormButton';
 import Navbar from 'components/Navbar';
+import { getAccessToken } from 'api/Auth';
+import styles from './../Auth.module.scss';
 
 function SignInPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const checkIsValid = () => {
+    if (email === '') {
+      setErrorMessage('이메일을 입력해 주세요');
+      return false;
+    }
+
+    if (password === '') {
+      setErrorMessage('비밀번호를 입력해 주세요');
+      return false;
+    }
+
+    setErrorMessage('');
+    return true;
+  };
+
+  const submitForm = async () => {
+    if (checkIsValid()) {
+      try {
+        const accessToken = await getAccessToken(email, password);
+        localStorage.setItem('accessToken', accessToken);
+        navigate('/');
+      } catch (error) {
+        setErrorMessage('에러가 발생하였습니다 (임시문구)');
+      }
+    }
+  };
+
   return (
     <>
       <Navbar />
       <div className={styles.content}>
         <div className={styles.content__signin__box}>
           <span className={styles.content__title}>SIGN IN</span>
-          <FormBox type="text" placeholder="Email" />
-          <FormBox type="password" placeholder="Password" />
+          <FormBox
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            placeholder="Email"
+          />
+          <FormBox
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="Password"
+          />
+          <div className={styles.error}>{errorMessage}</div>
           <div className={styles.button_wapper}>
-            <FormButton url="/" text="SIGN IN" />
-            <FormButton url="/signup" text="SIGN UP" />
+            <FormButton clickAction={submitForm} text="SIGN IN" />
+            <FormButton
+              clickAction={() => navigate('/signup')}
+              text="SIGN UP"
+            />
           </div>
         </div>
       </div>

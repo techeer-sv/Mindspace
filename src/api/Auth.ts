e478309458bb2b8a1c00de25af0957a5518b20eb
teawon <xodnjsvkvk9@gmail.com>
2023-05-14 01:37:43 +0900

    feat : 로그인, 회원가입 api 생성

---
+++ src/api/Auth.ts
@@ -0,0 +1,32 @@
+import axios from 'utils/axios';
+
+/**
+ * //TODO
+ * React-query를 사용해 컴포넌트에서 직접 api를 호출하지 않도록 수정해야합니다.
+ */
+
+export const createUser = async (
+  userName: string,
+  email: string,
+  password: string,
+) => {
+  await axios.post('user/signup', {
+    userName: userName,
+    email: email,
+    password: password,
+  });
+};
+
+export const getAccessToken = async (email: string, password: string) => {
+  const response = await axios.post('user/login', {
+    email: email,
+    password: password,
+  });
+
+  /**
+   * //FIXME
+   * data.id값으로 받아오는 응답값을 향후 백엔드 업데이트에 맞춰 accessToken값으로 수정해야합니다.
+   */
+  const accessToken = response.data.id;
+  return accessToken;
+};
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,17 +1,12 @@
 import axios from 'axios';
 
-const BASEURL = `${process.env.REACT_APP_IP}/api/v1/`;
-// 향후 BASEURL부분은 env파일로 분리할 것
+const BASEURL = `localhost:8080/api/v1/`;
+//TODO 향후 BASEURL부분은 env파일로 분리할 것
 
 const instance = axios.create({
   baseURL: BASEURL,
 });
 
-/**
- * 작성자 : 태원
- * 날짜 : 4/20
- * 내용 : 현재는 accessToken정보에 userId값을 담고 있지만 추후 실제 발급받은 토큰 정보를 보내야합니다.
- */
 instance.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem('accessToken');

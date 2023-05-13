3d6cc00364ac075acca5b73b5f0a5bc53884b2b2
teawon <xodnjsvkvk9@gmail.com>
2023-05-14 03:25:54 +0900

    feat : 회원가입 api 연결 및 state수정

--- src/pages/Auth/SignUpPage/index.tsx
+++ src/pages/Auth/SignUpPage/index.tsx
@@ -2,11 +2,58 @@ import styles from './../Auth.module.scss';
 import FormBox from '../components/FormBox';
 import FormButton from '../components/FormButton';
 import Navbar from 'components/Navbar';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { createUser } from 'api/Auth';
 
 function SignUpPage() {
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>('');
+  const [userName, setUserName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const checkIsValid = () => {
+    if (email === '') {
+      setErrorMessage('이메일을 입력해 주세요');
+      return false;
+    }
+
+    if (userName === '') {
+      setErrorMessage('닉네임을 입력해 주세요');
+      return false;
+    }
+
+    if (password === '') {
+      setErrorMessage('비밀번호를 입력해 주세요');
+      return false;
+    }
+    if (passwordConfirm === '') {
+      setErrorMessage('비밀번호 확인을 입력해 주세요');
+      return false;
+    }
+
+    if (password !== passwordConfirm) {
+      setErrorMessage('재확인 비밀번호가 일치하지 않습니다.');
+      return false;
+    }
+
+    setErrorMessage('');
+    return true;
+  };
+
+  const submitForm = async () => {
+    if (checkIsValid()) {
+      try {
+        await createUser(userName, email, password);
+        navigate('/signin');
+      } catch (error) {
+        setErrorMessage('에러가 발생하였습니다 (임시문구)');
+      }
+    }
+  };
 
   return (
     <>
@@ -14,8 +61,22 @@ function SignUpPage() {
       <div className={styles.content}>
         <div className={styles.content__signup__box}>
           <span className={styles.content__title}>Create Account</span>
-          <FormBox type="text" placeholder="Email" />
-          <FormBox type="text" placeholder="Nickname" />
+          <FormBox
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            placeholder="Email"
+          />
+          <FormBox
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            type="text"
+            placeholder="Nickname"
+          />
           <FormBox
             type="password"
             value={password}
@@ -32,14 +93,9 @@ function SignUpPage() {
             }}
             placeholder="Password Check"
           />
-          <div className={styles.confirm}>
-            {password !== passwordConfirm && (
-              <div>비밀번호가 일치하지 않습니다!</div>
-            )}
-            {password === passwordConfirm && <div>비밀번호 일치</div>}
-          </div>
+          <div className={styles.error}>{errorMessage}</div>
           <div className={styles.button_wapper}>
-            <FormButton url="/signin" text="SIGN UP" />
+            <FormButton text="SIGN UP" clickAction={submitForm} />
           </div>
         </div>
       </div>

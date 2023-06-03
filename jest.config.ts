27636fd8788462987cda30d1851dd26f99718b89
teawon <xodnjsvkvk9@gmail.com>
2023-06-04 02:04:53 +0900

    chore : Jest 이미지, style파일 모킹처리

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,19 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+export default {
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '^@/(.*)$': '<rootDir>/src/$1',
+
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/assetsTransformer.js',
+    '\\.(css|less|scss)$': '<rootDir>/assetsTransformer.ts',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!.*\\.(js|jsx|ts|tsx)$)',
+    '/path/to/image/files/.*',
+  ],
 };

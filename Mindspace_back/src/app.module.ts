79b7b95f105add200eb349ab9ad9c4058eab4b2f
suhyeon <suhyeon3484@naver.com>
2023-09-11 23:36:17 +0900

    set: DB 연동

--- Mindspace_back/src/app.module.ts
+++ Mindspace_back/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전체적으로 사용
+      cache: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true, // 개발 환경에서만 true로 설정
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

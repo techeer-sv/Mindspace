903fad17049413333e8a17fc791508157267c2a9
suhyeon <suhyeon3484@naver.com>
2023-09-12 01:24:14 +0900

    set: swagger 추가

--- Mindspace_back/src/main.ts
+++ Mindspace_back/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Mindspace API')
+    .setDescription('Mindspace API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(8080);
 }
 bootstrap();

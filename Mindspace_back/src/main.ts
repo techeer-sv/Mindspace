c57bd9d3dd6e58c89f6101f1f6a304d4a474f545
suhyeon <suhyeon3484@naver.com>
2023-09-12 01:25:33 +0900

    set: CORS 설정 추가

--- Mindspace_back/src/main.ts
+++ Mindspace_back/src/main.ts
@@ -3,15 +3,26 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    },
+  });
+
   const options = new DocumentBuilder()
     .setTitle('Mindspace API')
     .setDescription('Mindspace API description')
     .setVersion('1.0')
     .addTag('API')
     .build();
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
+
   await app.listen(8080);
 }
 bootstrap();

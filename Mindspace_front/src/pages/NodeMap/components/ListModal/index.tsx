72f067027542c5606ad431bf032c789070760de1
choiseyeon <choi20014830@gmail.com>
2023-09-24 02:05:55 +0900

    feat: 댓글 모달 애니메이션 추가

--- Mindspace_front/src/pages/NodeMap/components/ListModal/index.tsx
+++ Mindspace_front/src/pages/NodeMap/components/ListModal/index.tsx
@@ -12,6 +12,10 @@ function ListModal({ listModalOpen, onListRequestClose }: ListModalProps) {
   const [isSelectedTable, setIsSelectedTable] = useState(null);
   const viewerRef = useRef(null);
   const { data: postData, isLoading } = usePostGetQuery(isSelectedTable);
+  const [commentModalOpen, setCommentModalOpen] = useState(false);
+  const toggleCommentModal = () => {
+    setCommentModalOpen(prev => !prev);
+  };
 
   const handleSelecteBoard = (id: number) => {
     setIsSelectedTable(id);
@@ -53,34 +57,40 @@ function ListModal({ listModalOpen, onListRequestClose }: ListModalProps) {
             >
               <span className={styles.post__button}>Back</span>
             </button>
-            <div className={styles.post__content}>
-              <div className={styles.post__content__wrapper}>
-                <div className={styles.post__content__wrapper__header}>
-                  <div className={styles.post__content__wrapper__header__text}>
+            <div className={styles.post__wrapper}>
+            <div className={styles.post__wrapper__content}>
+              <div className={styles.post__wrapper__content__wrapper}>
+                <div className={styles.post__wrapper__content__wrapper__header}>
+                  <div className={styles.post__wrapper__content__wrapper__header__text}>
                     <span
                       className={
-                        styles.post__content__wrapper__header__text__title
+                        styles.post__wrapper__content__wrapper__header__text__title
                       }
                     >
                       {postData?.title}
                     </span>
                   </div>
-                  <div className={styles.post__content__wrapper__info}>
-                    <span className={styles.post__content__wrapper__info__name}>
+                  <div className={styles.post__wrapper__content__wrapper__info}>
+                    <button onClick={toggleCommentModal} className={styles.post__wrapper__content__wrapper__info__button}>댓글</button>
+                    <span className={styles.post__wrapper__content__wrapper__info__name}>
                       {postData.userNickname}
                     </span>
-                    <span className={styles.post__content__wrapper__info__date}>
+                    <span className={styles.post__wrapper__content__wrapper__info__date}>
                       {formatDateTime(postData.updatedAt, DateTimeFormat.Date)}
                     </span>
                   </div>
                 </div>
-                <div className={styles.post__viewer}>
+                <div className={styles.post__wrapper__viewer}>
                   <Viewer
                     ref={viewerRef}
                     initialValue={postData?.content}
                     usageStatistics={false}
                   />
                 </div>
+              </div>
+            </div>
+              <div className={commentModalOpen ? styles.commentModal : styles.hiddenCommentModal}>
+                <span>댓글 내용</span>
               </div>
             </div>
           </>

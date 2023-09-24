7748c060e4704525d3ebd8f04362330b1bbcb076
Seyeon_Choi <choi20014830@gmail.com>
2023-09-25 03:44:54 +0900

    feat: 댓글 모달 추가

--- Mindspace_front/src/pages/NodeMap/components/ListModal/index.tsx
+++ Mindspace_front/src/pages/NodeMap/components/ListModal/index.tsx
@@ -7,6 +7,8 @@ import CustomModal from '@/components/CustomModal';
 import { usePostGetQuery } from '@/hooks/queries/board';
 
 import { formatDateTime, DateTimeFormat } from '@/utils/dateTime';
+import CommentIcon from '../../../../asset/icon/Comment.svg';
+import SendCommentIcon from '../../../../asset/icon/SendComment.svg';
 
 function ListModal({ listModalOpen, onListRequestClose }: ListModalProps) {
   const [isSelectedTable, setIsSelectedTable] = useState(null);
@@ -71,7 +73,9 @@ function ListModal({ listModalOpen, onListRequestClose }: ListModalProps) {
                     </span>
                   </div>
                   <div className={styles.post__wrapper__content__wrapper__info}>
-                    <button onClick={toggleCommentModal} className={styles.post__wrapper__content__wrapper__info__button}>댓글</button>
+                      <div className={styles.post__wrapper__content__wrapper__info__box}>
+                        <button onClick={toggleCommentModal} className={styles.post__wrapper__content__wrapper__info__box__button}>댓글</button>
+                      </div>
                     <span className={styles.post__wrapper__content__wrapper__info__name}>
                       {postData.userNickname}
                     </span>
@@ -79,6 +83,7 @@ function ListModal({ listModalOpen, onListRequestClose }: ListModalProps) {
                       {formatDateTime(postData.updatedAt, DateTimeFormat.Date)}
                     </span>
                   </div>
+
                 </div>
                 <div className={styles.post__wrapper__viewer}>
                   <Viewer
@@ -89,8 +94,38 @@ function ListModal({ listModalOpen, onListRequestClose }: ListModalProps) {
                 </div>
               </div>
             </div>
-              <div className={commentModalOpen ? styles.commentModal : styles.hiddenCommentModal}>
-                <span>댓글 내용</span>
+              <div className={commentModalOpen ? styles.post__wrapper__comment : styles.post__wrapper__hiddencomment}>
+                <div className={styles.post__wrapper__comment__header}>
+                    <div className={styles.post__wrapper__comment__header__icon}>
+                      <img src={CommentIcon} alt="Comment Icon" />
+                    </div>
+                    <span className={styles.post__wrapper__comment__header__text}>댓글 3</span>
+                </div>
+                <div className={styles.post__wrapper__comment__input}>
+                  <input type="text" placeholder="Enter your comment" />
+                    <div className={styles.post__wrapper__comment__input__icon}>
+                      <button>
+                        <img src={SendCommentIcon} alt="Comment Icon" />
+                      </button>
+                    </div>
+                </div>
+                <div className={styles.post__wrapper__comment__content}>
+                  <div className={styles.post__wrapper__comment__content__nickname}>
+                    <span className={styles.post__wrapper__comment__content__nickname__text}>
+                      바라밥밥
+                    </span>
+                  </div>
+                  <div className={styles.post__wrapper__comment__content__box}>
+                    <span className={styles.post__wrapper__comment__content__box__text}>
+                      잘 쓰셨네요.
+                    </span>
+                  </div>
+                  <div className={styles.post__wrapper__comment__content__time}>
+                    <span className={styles.post__wrapper__comment__content__time__text}>
+                      5분전
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           </>

c356c10fa8d52823a0a6f3d0ec1002995d94c64f
teawon <xodnjsvkvk9@gmail.com>
2023-09-28 17:56:58 +0900

    chore : Next기본 설정

---
+++ Mindspace_front_next/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h4 className="title">테스트</h4>
+    </div>
+  );
+}

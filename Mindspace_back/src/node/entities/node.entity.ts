0029719323e5c8f8a1ca9e97b69da13fd2d94149
suhyeon <suhyeon3484@naver.com>
2023-09-29 19:30:19 +0900

    fix: eslint/prettier 문법에 맞도록 수정

--- Mindspace_back/src/node/entities/node.entity.ts
+++ Mindspace_back/src/node/entities/node.entity.ts
@@ -1,13 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Timestamp } from '../../global/common/timeStamp';
 
 @Entity()
 export class Node extends Timestamp {
+  @PrimaryGeneratedColumn('increment', { name: 'node_id' })
+  id: number;
 
-    @PrimaryGeneratedColumn('increment', { name: 'node_id' })
-    id: number;
-
-    @Column({ name: 'node_name', type: 'varchar', nullable: false })
-    name: string;
-
+  @Column({ name: 'node_name', type: 'varchar', nullable: false })
+  name: string;
 }

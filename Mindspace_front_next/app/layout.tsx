bd9bffe92b51c3b495a4f27c4c124627cc4d1625
teawon <xodnjsvkvk9@gmail.com>
2023-09-29 19:40:36 +0900

    feat : favicon 수정 및 MetaData내용 추가

--- Mindspace_front_next/app/layout.tsx
+++ Mindspace_front_next/app/layout.tsx
@@ -9,6 +9,19 @@ export const metadata: Metadata = {
   title: "Mindspace",
   description:
     "Mindspace는 개발에 관련된 각 키워드를 노드와 노드 사이의 연결 관계로 표현하여 사용자에게 학습 방향을 제공하는 사이트입니다.",
+  keywords: "개발, 프로그래밍, 학습, 연결 관계, 노드, 키워드",
+  openGraph: {
+    title: "Mindspace",
+    description:
+      "Mindspace는 개발에 관련된 각 키워드를 노드와 노드 사이의 연결 관계로 표현하여 사용자에게 학습 방향을 제공하는 사이트입니다.",
+    // url : localhost:3000/
+    images: [
+      {
+        url: "/images/logo.png",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -18,6 +31,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <LayoutProvider>{children}</LayoutProvider>
       </body>

15eb760d6ed3fff7904402db210b63893505c035
teawon <xodnjsvkvk9@gmail.com>
2023-09-29 19:45:27 +0900

    fix: metadataBase 경고 수정

--- Mindspace_front_next/app/layout.tsx
+++ Mindspace_front_next/app/layout.tsx
@@ -10,11 +10,11 @@ export const metadata: Metadata = {
   description:
     "Mindspace는 개발에 관련된 각 키워드를 노드와 노드 사이의 연결 관계로 표현하여 사용자에게 학습 방향을 제공하는 사이트입니다.",
   keywords: "개발, 프로그래밍, 학습, 연결 관계, 노드, 키워드",
+  metadataBase: new URL("http://localhost:3000"), //FIXME : 향후 실제 도메인으로 수정
   openGraph: {
     title: "Mindspace",
     description:
       "Mindspace는 개발에 관련된 각 키워드를 노드와 노드 사이의 연결 관계로 표현하여 사용자에게 학습 방향을 제공하는 사이트입니다.",
-    // url : localhost:3000/
     images: [
       {
         url: "/images/logo.png",

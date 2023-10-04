6a93327b168cab2b26ad0e5ea21c0cf852d6ce02
Seyeon_Choi <choi20014830@gmail.com>
2023-10-04 22:55:05 +0900

    fix: 파라미터 네이밍 변경

--- Mindspace_back/src/board/board.controller.ts
+++ Mindspace_back/src/board/board.controller.ts
@@ -45,14 +45,14 @@ export class BoardController {
   }
 
   @ApiOperation({ summary: '게시글 생성' })
-  @ApiQuery({ name: 'node_id', description: '게시글을 작성할 노드의 ID' })
-  @ApiHeader({ name: 'Authorization', description: '사용자 ID' })
+  @ApiQuery({ name: 'node_id', description: '게시글을 작성할 노드의 ID'})
+  @ApiHeader({ name: 'user_id', description: '사용자 ID'})
   @ApiResponse({ status: 201, description: '게시글 작성 성공', type: Board }) // 201 Created response
   @ApiResponse({ status: 500, description: '서버 오류' }) // 500 Internal Server Error response
   @Post()
   async createBoard(
     @Query('node_id') nodeId: number,
-    @Headers('Authorization') userIdHeader: string,
+    @Headers('user_id') userIdHeader: string,
     @Body() createBoardDto: CreateBoardDto,
   ): Promise<BoardResponseDto> {
     // <-- 변경된 반환 타입
@@ -62,13 +62,13 @@ export class BoardController {
 
   @ApiOperation({ summary: '게시글 수정' })
   @ApiQuery({ name: 'node_id', description: '게시글을 작성할 노드의 ID' })
-  @ApiHeader({ name: 'Authorization', description: '사용자 ID' })
+  @ApiHeader({ name: 'user_id', description: '사용자 ID' })
   @ApiResponse({ status: 201, description: '게시글 작성 성공', type: Board }) // 201 Created response
   @ApiResponse({ status: 500, description: '서버 오류' }) // 500 Internal Server Error response
   @Put()
   async updateBoard(
     @Query('node_id') nodeId: number,
-    @Headers('Authorization') userIdHeader: string,
+    @Headers('user_id') userIdHeader: string,
     @Body() updateBoardDto: UpdateBoardDto,
   ): Promise<BoardResponseDto> {
     // <-- 변경된 반환 타입
@@ -80,7 +80,7 @@ export class BoardController {
   @Delete()
   async deleteOwnBoard(
     @Query('node_id') nodeId: number,
-    @Headers('Authorization') userId: string,
+    @Headers('user_id') userId: string,
   ) {
     return this.boardService.deleteOwnBoard(nodeId, userId);
   }
@@ -93,7 +93,7 @@ export class BoardController {
     type: Number,
   })
   @ApiHeader({
-    name: 'Authorization',
+    name: 'user_id',
     description: '사용자 ID',
     required: true,
   })
@@ -106,7 +106,7 @@ export class BoardController {
   @Get()
   async getBoardByNodeId(
     @Query('node_id') nodeId: number,
-    @Headers('Authorization') userIdHeader: string,
+    @Headers('user_id') userIdHeader: string,
   ): Promise<SpecificBoardNodeDto> {
     const userId = parseInt(userIdHeader);
     return await this.boardService.getBoardByNodeIdAndUserId(nodeId, userId);

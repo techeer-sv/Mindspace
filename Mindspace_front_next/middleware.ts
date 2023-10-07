23d2f3971df5109b1c6eefb2bd8abcdcc69058fc
teawon <xodnjsvkvk9@gmail.com>
2023-10-08 05:09:22 +0900

    chore : 불필요한 코드 삭제

--- Mindspace_front_next/middleware.ts
+++ Mindspace_front_next/middleware.ts
@@ -5,7 +5,6 @@ export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|fonts|images).*)"],
 };
 
-const unprotectedRoutes = ["/"]; // 검증이 필요 없는 경로 목록
 const protectedRoutes = ["/map"]; // 로그인이 필요한 페이지 목록
 const publicRoutes = ["/signin", "/signup"]; // 로그인이 되면 접근할 수 없는 페이지 목록
 
@@ -15,13 +14,13 @@ export function middleware(request: NextRequest) {
 
   if (!token && protectedRoutes.includes(currentPath)) {
     const url = request.nextUrl.clone();
-    url.pathname = "/signin"; //
+    url.pathname = "/signin";
     return NextResponse.redirect(url);
   }
 
   if (token && publicRoutes.includes(currentPath)) {
     const url = request.nextUrl.clone();
-    url.pathname = "/"; //
+    url.pathname = "/";
     return NextResponse.redirect(url);
   }
 

9ce7908d82ae6cd7f2ee86ae97a10180de3e5295
suhyeon <suhyeon3484@naver.com>
2023-10-09 18:07:03 +0900

    chore: board exception prettier, eslint 포맷팅

--- Mindspace_back/src/board/exception/NodeNotFoundException.ts
+++ Mindspace_back/src/board/exception/NodeNotFoundException.ts
@@ -1,11 +1,13 @@
-
-import {HttpStatus} from "@nestjs/common";
-import {CustomException} from "../../global/common/customException";
-import {ErrorCode} from "../../global/exception/ErrorCode";
-
+import { HttpStatus } from '@nestjs/common';
+import { CustomException } from '../../global/common/customException';
+import { ErrorCode } from '../../global/exception/ErrorCode';
 
 export class NodeNotFoundException extends CustomException {
-    constructor() {
-        super(ErrorCode.NODE_NOT_FOUND, "해당 노드를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
-    }
+  constructor() {
+    super(
+      ErrorCode.NODE_NOT_FOUND,
+      '해당 노드를 찾을 수 없습니다.',
+      HttpStatus.NOT_FOUND,
+    );
+  }
 }

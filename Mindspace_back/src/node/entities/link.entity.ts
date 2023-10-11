989593c5315c68bc409e15a0993d591f2198ea92
zyoon <develop_z@naver.com>
2023-10-12 00:29:40 +0900

    feat: neo4j entity 작업 *미완성

---
+++ Mindspace_back/src/node/entities/link.entity.ts
@@ -0,0 +1,41 @@
+// import { PrimaryGeneratedColumn  } from 'typeorm';
+// import { RelationshipEntity, Property } from 'nest-neo4j';
+//
+// @RelationshipEntity(type => 'LINKS_TO')
+// export class Link {
+//     @PrimaryGeneratedColumn('increment')
+//     id: number;
+//
+//     @Property({ name: 'source' })
+//     source: number;
+//
+//     @Property({ name: 'target' })
+//     target: number;
+// }
+// import { NodeEntity, PrimaryGeneratedColumn, Property } from 'nest-neo4j';
+//
+// @NodeEntity('Link')
+// export class Link {
+//     @PrimaryGeneratedColumn()
+//     id: number;
+//
+//     @Property()
+//     source: number;
+//
+//     @Property()
+//     target: number;
+// }
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Link {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    source: number;
+
+    @Column()
+    target: number;
+}

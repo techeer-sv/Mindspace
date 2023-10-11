edbc28c0176ef8524378ea7a25036e80fa6626d0
zyoon <develop_z@naver.com>
2023-10-12 00:31:52 +0900

    feat: node에서 사용하는 neo4j 서비스, 컨트롤러 작업

--- Mindspace_back/src/node/node.controller.ts
+++ Mindspace_back/src/node/node.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { NodeService } from './node.service'; 
+import { Controller, Get, Headers } from '@nestjs/common';
+import { NodeService } from './node.service';
 import { Node } from './entities/node.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {NodeInfoResponse} from "./dto/node-info-response.dto";
+import {NodeResponseDto} from "./dto/node-response.dto";
 
+@ApiTags('Node')
 @Controller('api/v1/node')
 export class NodeController {
-    constructor(private readonly nodeService: NodeService) {}
+  constructor(private readonly nodeService: NodeService) {}
 
-    @Get()
-    async getAllNode(): Promise<Node[]> {
-        return this.nodeService.getAllNode();
-    }
+  @ApiOperation({ summary: '전체 노드 조회' })
+  @Get('all')
+  async getAllNode(): Promise<NodeResponseDto[]> {
+    return this.nodeService.getAllNode();
+  }
+
+  @Get('check')
+  async getNodeInfoWithLinks(@Headers('authorization') userId: number): Promise<NodeInfoResponse> {
+    const nodeInfo = await this.nodeService.getNodeInfoWithLinks(userId);
+    return nodeInfo;
+  }
+
 }
--- Mindspace_back/src/node/node.service.ts
+++ Mindspace_back/src/node/node.service.ts
@@ -2,20 +2,55 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Node } from './entities/node.entity';
-import { NodeResponseDto } from "./dto/node-response.dto";
-import { NodeMapper } from "./dto/node.mapper";
+import {Neo4jNodeRepository} from "./neo4jNode.repository";
+import {NodeResponseDto} from "./dto/node-response.dto";
+import {NodeMapper} from "./dto/node.mapper";
+import {NodeInfoResponse} from "./dto/node-info-response.dto";
+import {Board} from "../board/entities/board.entity";
+import {nodeBoardRepository} from "./nodeBoard.repository";
+import {Neo4jNode} from "./entities/neo4jNode.entity";
 
 @Injectable()
 export class NodeService {
     constructor(
         @InjectRepository(Node)
             private readonly nodeRepository: Repository<Node>,
+        @InjectRepository(Board)
+            private readonly nodeBoardRespository: nodeBoardRepository,
+        @InjectRepository(Neo4jNode)
+            private readonly neo4jNodeRepository: Neo4jNodeRepository,
             private readonly nodeMapper: NodeMapper
     ) {}
 
     async getAllNode(): Promise<NodeResponseDto[]> {
         const nodes = await this.nodeRepository.find();
         return nodes.map(node => this.nodeMapper.DtoFromEntity(node));
+    }
+
+    async getNodeInfoWithLinks(userId: number): Promise<NodeInfoResponse> {
+        const nodes = await this.nodeBoardRespository.getNodeListWithWriteStatus(userId);
+        const links = await this.neo4jNodeRepository.findAllLinks();
+
+        // connectCount 계산 및 추가
+        const connectCounts: Map<number, number> = new Map();
+        links.forEach((link) => {
+            connectCounts.set(link.source, (connectCounts.get(link.source) || 0) + 1);
+            connectCounts.set(link.target, (connectCounts.get(link.target) || 0) + 1);
+        });
+
+        for (const node of nodes) {
+            const nodeId = node.id;
+            const count = connectCounts.get(nodeId) || 0;
+            node['connectCount'] = count;
+        }
+
+        const linkDTOs = links.map((link) => this.nodeMapper.linkToDto(link));
+
+        return {
+            nodes,
+            links: linkDTOs,
+        };
+
     }
 
 }

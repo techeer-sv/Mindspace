03eca00071225960236588f1e9aed428c9079e18
teawon <xodnjsvkvk9@gmail.com>
2023-10-14 06:22:13 +0900

    feat : 노드맵 api SSR적용

---
+++ Mindspace_front_next/app/map/hydratedNode.tsx
@@ -0,0 +1,19 @@
+import { dehydrate } from "@tanstack/react-query";
+import getQueryClient from "@/getQueryClient";
+import { getNodeList } from "@/api/node";
+import { NODE_QUERIES } from "@/constants/queryKeys";
+import MapPage from "./map";
+import HydrateOnClient from "@/hydrateOnClient";
+
+export default async function HydratedNode() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery([NODE_QUERIES.LIST], getNodeList);
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrateOnClient state={dehydratedState}>
+      <MapPage />
+    </HydrateOnClient>
+  );
+}

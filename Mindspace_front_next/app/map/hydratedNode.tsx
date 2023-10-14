535d6846f53c0afeeb687e3b06fe4d1eeba9a55e
teawon <xodnjsvkvk9@gmail.com>
2023-10-14 20:36:32 +0900

    fix : 동적 import를 통한 ReferenceError 수정

--- Mindspace_front_next/app/map/hydratedNode.tsx
+++ Mindspace_front_next/app/map/hydratedNode.tsx
@@ -2,13 +2,16 @@ import { dehydrate } from "@tanstack/react-query";
 import getQueryClient from "@/getQueryClient";
 import { getNodeListSSR } from "@/api/nodeServer";
 import { NODE_QUERIES } from "@/constants/queryKeys";
-import MapPage from "./map";
 import HydrateOnClient from "@/hydrateOnClient";
+import dynamic from "next/dynamic";
 
 export default async function HydratedNode() {
+  const MapPage = dynamic(() => import("./map"), {
+    ssr: false,
+  });
+
   const queryClient = getQueryClient();
   await queryClient.prefetchQuery([NODE_QUERIES.LIST], getNodeListSSR);
-
   const dehydratedState = dehydrate(queryClient);
 
   return (

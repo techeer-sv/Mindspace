3fe902ffd7deff61c987c2891d0f98aef800eda8
teawon <xodnjsvkvk9@gmail.com>
2023-10-22 19:39:45 +0900

    fix : Header 변수명 user_id로 수정

--- Mindspace_front_next/app/api/utils/csrFetch.ts
+++ Mindspace_front_next/app/api/utils/csrFetch.ts
@@ -8,8 +8,9 @@ export const csrFetch = async <T = any>(
   const accessToken = Cookies.get("accessToken");
   const authHeader: Record<string, string> = {};
 
+  // FIXME Swagger이슈로 인해 향후 Authoization Header를 사용하게되면 수정해야합니다
   if (accessToken) {
-    authHeader.Authorization = `${accessToken}`;
+    authHeader.user_id = `${accessToken}`;
   }
 
   return baseFetch<T>(endpoint, authHeader, options);

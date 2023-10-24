893c03967fc3a4a9ba766203ad5979c3aa18b484
suhyeon <suhyeon3484@naver.com>
2023-10-25 04:30:44 +0900

    fix: 초기 데이터 삽입 문제 해결

--- Mindspace_back/src/main.ts
+++ Mindspace_back/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './global/common/globalExceptionFilter';
+import { NodeService } from './node/node.service';
+// import { GlobalExceptionFilter } from './global/common/globalExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -24,7 +25,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
-  app.useGlobalFilters(new GlobalExceptionFilter());
+  // FIXME: GlobalExceptionFilter를 사용하면 일부 예외처리가 되지 않아 해결 필요
+  // app.useGlobalFilters(new GlobalExceptionFilter());
+  const nodeService = app.get(NodeService);
+  await nodeService.seedInitialData();
   await app.listen(8000);
 }
 bootstrap();

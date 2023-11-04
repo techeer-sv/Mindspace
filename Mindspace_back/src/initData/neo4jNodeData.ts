d584343ca32fec97df3bc1b308d8cb4de001b3ae
suhyeon <suhyeon3484@naver.com>
2023-11-04 23:12:15 +0900

    refactor: neo4j, node repository 추가

---
+++ Mindspace_back/src/initData/neo4jNodeData.ts
@@ -0,0 +1,154 @@
+export const nodeNames = [
+  'Javascript',
+  'Webpack',
+  'React',
+  'JavaScript Immutability',
+  'Ajax',
+  'React Route',
+  'State Management',
+  'Class vs Component',
+  'React Query',
+  'Jest',
+  'ES6',
+  'Redux',
+  'Mobx',
+  'Zustand',
+  'Recoil',
+  'Redux saga',
+  'Redux toolkit',
+  'Browser Render',
+  'HTML',
+  'Proxy',
+  'Auth',
+  'OAuth2.0',
+  'FaceBook Login',
+  'Kakao Login',
+  'Bearer Auth',
+  'JWT Token',
+  'CSS',
+  'CSS vs CSS in JS',
+  'SASS',
+  'Tailwind',
+  'Bootstrap',
+  'Styled-compoent',
+  'Material-UI',
+];
+
+export const nodeLinks = [
+  {
+    source: 1,
+    target: 2,
+  },
+  {
+    source: 1,
+    target: 4,
+  },
+  {
+    source: 1,
+    target: 5,
+  },
+  {
+    source: 3,
+    target: 6,
+  },
+  {
+    source: 3,
+    target: 7,
+  },
+  {
+    source: 3,
+    target: 8,
+  },
+  {
+    source: 3,
+    target: 9,
+  },
+  {
+    source: 3,
+    target: 10,
+  },
+  {
+    source: 1,
+    target: 11,
+  },
+  {
+    source: 7,
+    target: 12,
+  },
+  {
+    source: 7,
+    target: 13,
+  },
+  {
+    source: 7,
+    target: 14,
+  },
+  {
+    source: 7,
+    target: 15,
+  },
+  {
+    source: 12,
+    target: 16,
+  },
+  {
+    source: 12,
+    target: 17,
+  },
+  {
+    source: 18,
+    target: 19,
+  },
+  {
+    source: 5,
+    target: 20,
+  },
+  {
+    source: 21,
+    target: 22,
+  },
+  {
+    source: 21,
+    target: 25,
+  },
+  {
+    source: 21,
+    target: 26,
+  },
+  {
+    source: 22,
+    target: 23,
+  },
+  {
+    source: 22,
+    target: 24,
+  },
+  {
+    source: 27,
+    target: 19,
+  },
+  {
+    source: 27,
+    target: 28,
+  },
+  {
+    source: 27,
+    target: 29,
+  },
+  {
+    source: 27,
+    target: 30,
+  },
+  {
+    source: 27,
+    target: 31,
+  },
+  {
+    source: 27,
+    target: 32,
+  },
+  {
+    source: 27,
+    target: 33,
+  },
+];
--- Mindspace_back/src/node/node.module.ts
+++ Mindspace_back/src/node/node.module.ts
@@ -5,10 +5,18 @@ import { NodeController } from './node.controller';
 import { NodeService } from './node.service';
 import { NodeMapper } from './dto/node.mapper';
 import { Neo4jNodeService } from '../neo4j-node/neo4j-node.service';
+import { CustomNodeRepository } from './repository/node.repository';
+import { CustomNeo4jNodeRepository } from '../neo4j-node/repository/neo4j-node.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Node])],
-  providers: [NodeService, NodeMapper, Neo4jNodeService],
+  providers: [
+    NodeService,
+    NodeMapper,
+    Neo4jNodeService,
+    CustomNodeRepository,
+    CustomNeo4jNodeRepository,
+  ],
   controllers: [NodeController],
   exports: [NodeService],
 })

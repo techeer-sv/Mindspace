c3377488348e66c0767e0d7ecef7b135f9e24a6c
suhyeon <suhyeon3484@naver.com>
2023-11-05 15:35:24 +0900

    fix: 외래키 제약 조건 되돌리기

--- Mindspace_back/src/notification/entities/notification.entity.ts
+++ Mindspace_back/src/notification/entities/notification.entity.ts
@@ -22,8 +22,8 @@ export class Notification {
   user_id: number;
 
   // eager를 true로 설정하여 자동으로 관련된 Board 정보를 가져올 수 있게 합니다.
-  // 외래키 제약 조건 수정
-  @ManyToOne(() => Board, { eager: true, onDelete: 'CASCADE' })
+  // TODO: 삭제된 게시글일 경우에는 "삭제된 게시글"로 수정
+  @ManyToOne(() => Board, { eager: true })
   @JoinColumn({ name: 'board_id' })
   board: Board;
 

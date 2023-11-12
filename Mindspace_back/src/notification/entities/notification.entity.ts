e00a0d6ad182645b6830289645fb3883c0eb9511
suhyeon <suhyeon3484@naver.com>
2023-11-12 19:02:23 +0900

    fix: 게시글 삭제 soft delete로 수정

--- Mindspace_back/src/notification/entities/notification.entity.ts
+++ Mindspace_back/src/notification/entities/notification.entity.ts
@@ -23,7 +23,7 @@ export class Notification {
 
   // eager를 true로 설정하여 자동으로 관련된 Board 정보를 가져올 수 있게 합니다.
   // TODO: 삭제된 게시글일 경우에는 "삭제된 게시글"로 수정
-  @ManyToOne(() => Board, { eager: true })
+  @ManyToOne(() => Board, { eager: true, onDelete: 'SET NULL' })
   @JoinColumn({ name: 'board_id' })
   board: Board;
 

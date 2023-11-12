ca9515ec832a8229d6365ed95d9d4f1cc0f9dddd
ssojungg <kawy.sojung@gmail.com>
2023-11-12 15:13:27 +0900

    fix:notification entitiy의 board 불필요한 주석 제거

--- Mindspace_back/src/notification/entities/notification.entity.ts
+++ Mindspace_back/src/notification/entities/notification.entity.ts
@@ -28,11 +28,7 @@ export class Notification {
   @ApiProperty({ description: '게시글을 작성한 사용자' })
   user: User;
 
-  @ManyToOne(() => Board, { eager: true }) // eager를 true로 설정하여 자동으로 관련된 Board 정보를 가져올 수 있게 합니다.
+  @ManyToOne(() => Board)
   @JoinColumn({ name: 'board_id' })
   board: Board;
-
-  get userNickname(): string {
-    return this.board.user.nickname; // board 객체를 통해 user의 nickname을 가져옵니다. 이때 board의 user 객체는 eager 로딩되어 있어야 합니다.
-  }
 }

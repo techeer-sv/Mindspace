6168f0ac589fdafd2a1ed310d20c716abc7191d2
zyoon <develop_z@naver.com>
2023-11-15 23:03:16 +0900

    feat: node controller 테스트 코드 작성

---
+++ Mindspace_back/src/node/node.controller.spec.ts
@@ -0,0 +1,94 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NodeController } from './node.controller';
+import { NodeService } from './node.service';
+
+const MockNodeService = {
+  getAllNode : jest.fn(),
+  getNodeInfoWithLinks : jest.fn(),
+}
+
+describe('NodeController', () => {
+  let controller: NodeController;
+  let nodeService: NodeService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [NodeController],
+      providers: [
+        { provide: NodeService, useValue: MockNodeService },
+      ],
+    }).compile();
+
+    controller = module.get<NodeController>(NodeController);
+    nodeService = module.get<NodeService>(NodeService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should return all nodes', async () => {
+    MockNodeService.getAllNode.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Node 1',
+      },
+      {
+        id: 2,
+        name: 'Node 2',
+      },
+    ]);
+
+    const nodes = await controller.getAllNode();
+
+    expect(nodes).toEqual([
+      {
+        id: 1,
+        name: 'Node 1',
+      },
+      {
+        id: 2,
+        name: 'Node 2',
+      },
+    ]);
+  });
+
+  it('should return node map info', async () => {
+    MockNodeService.getNodeInfoWithLinks.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Node 1',
+        links: [
+          {
+            id: 2,
+            name: 'Node 2',
+          },
+          {
+            id: 3,
+            name: 'Node 3',
+          },
+        ],
+      },
+    ]);
+
+    const nodes = await controller.getNodeInfoWithLinks(1);
+
+    expect(nodes).toEqual([
+      {
+        id: 1,
+        name: 'Node 1',
+        links: [
+          {
+            id: 2,
+            name: 'Node 2',
+          },
+          {
+            id: 3,
+            name: 'Node 3',
+          },
+        ],
+      },
+    ]);
+  });
+
+});

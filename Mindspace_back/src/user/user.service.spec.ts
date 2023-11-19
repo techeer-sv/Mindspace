16f73a457d1bd030d6c6b13599f408ed75a58875
Soyoon <spurt@tukorea.ac.kr>
2023-11-19 15:59:41 +0900

    feat: UserService의 signupUser에 대한 테스트 코드
    이 테스트는 이메일과 닉네임이 다른 사람과 겹치지 않을 때 새로운 사용자가 만들어지는지, 이미 존재하는 이메일이나 닉네임을 사용할 때 예외가 발생하는지 확인합니다.

--- Mindspace_back/src/user/user.service.spec.ts
+++ Mindspace_back/src/user/user.service.spec.ts
@@ -1,18 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { UserSignupRequestDto } from './dto/user-signup-request.dto';
+import { UserEmailDuplicatedException } from './exception/UserEmailDuplicatedException';
+import { UserNicknameDuplicatedException } from './exception/UserNicknameDuplicatedException';
+import { UserMapper } from './dto/user.mapper';
 
 describe('UserService', () => {
   let service: UserService;
+  let repo: Repository<User>;
+  let mapper: UserMapper;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: getRepositoryToken(User), useClass: Repository },
+        {
+          provide: UserMapper,
+          useValue: { DtoToEntity: jest.fn().mockReturnValue(new User()) },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repo = module.get<Repository<User>>(getRepositoryToken(User));
+    mapper = module.get<UserMapper>(UserMapper);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('signupUser', () => {
+    it('should create a user if email and nickname are unique', async () => {
+      const dto = new UserSignupRequestDto();
+      dto.email = 'test@test.com';
+      dto.nickname = 'testNickname';
+      jest.spyOn(repo, 'findOne').mockResolvedValue(null);
+      jest.spyOn(repo, 'save').mockResolvedValue(dto as any);
+
+      const result = await service.signupUser(dto);
+
+      expect(result).toEqual(dto);
+    });
+
+    it('should throw UserEmailDuplicatedException if email already exists', async () => {
+      const dto = new UserSignupRequestDto();
+      dto.email = 'test@test.com';
+      dto.nickname = 'testNickname';
+      jest.spyOn(repo, 'findOne').mockResolvedValueOnce(new User());
+
+      await expect(service.signupUser(dto)).rejects.toThrow(
+        UserEmailDuplicatedException,
+      );
+    });
+
+    it('should throw UserNicknameDuplicatedException if nickname already exists', async () => {
+      const dto = new UserSignupRequestDto();
+      dto.email = 'test@test.com';
+      dto.nickname = 'testNickname';
+      jest
+        .spyOn(repo, 'findOne')
+        .mockResolvedValueOnce(null)
+        .mockResolvedValueOnce(new User());
+
+      await expect(service.signupUser(dto)).rejects.toThrow(
+        UserNicknameDuplicatedException,
+      );
+    });
   });
 });

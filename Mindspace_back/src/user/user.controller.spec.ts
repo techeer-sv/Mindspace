7b08a7c08319bb43930e875beae63e690c2c0a4b
Soyoon <spurt@tukorea.ac.kr>
2023-11-20 01:24:28 +0900

    feat: UserController의 loginUser에 대한 테스트 코드

--- Mindspace_back/src/user/user.controller.spec.ts
+++ Mindspace_back/src/user/user.controller.spec.ts
@@ -5,6 +5,7 @@ import { UserMapper } from './dto/user.mapper';
 import { UserSignupRequestDto } from './dto/user-signup-request.dto';
 import { HttpStatus } from '@nestjs/common';
 import { User } from './entities/user.entity';
+import { UserLoginRequestDto } from './dto/user-login-request.dto';
 
 describe('UserController', () => {
   let userController: UserController;
@@ -20,6 +21,7 @@ describe('UserController', () => {
           provide: UserService,
           useValue: {
             signupUser: jest.fn(),
+            loginUser: jest.fn(),
           },
         },
         {
@@ -65,6 +67,38 @@ describe('UserController', () => {
 
       expect(res.status).toHaveBeenCalledWith(HttpStatus.CREATED);
       expect(res.json).toHaveBeenCalledWith(mappedResult);
+    });
+  });
+
+  describe('loginUser (로그인 API)', () => {
+    it('should return the expected result', async () => {
+      const loginResult = new User();
+      loginResult.id = 1;
+      loginResult.email = 'test@example.com';
+      loginResult.password = 'test';
+      loginResult.nickname = 'test';
+
+      const mappedResult = {
+        id: loginResult.id,
+        email: loginResult.email,
+        password: loginResult.password,
+        nickname: loginResult.nickname,
+      };
+
+      jest.spyOn(userService, 'loginUser').mockResolvedValue(loginResult);
+      jest.spyOn(userMapper, 'DtoFromEntity').mockReturnValue(mappedResult);
+
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const userLoginRequestDto: UserLoginRequestDto = {
+        email: 'test@example.com',
+        password: 'test',
+      };
+
+      await userController.loginUser(userLoginRequestDto, res as any);
+
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(res.json).toHaveBeenCalledWith(mappedResult);
+      expect(userService.loginUser).toHaveBeenCalledWith(userLoginRequestDto);
     });
   });
 });
